a4a7449d1f6e239be99f5b774b0fa3612743a3af
Colin le Roux <colinleroux@gmail.com>
2023-08-29 12:52:23 +0800

    login and jwt111

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ export default function () {
     baseURL: "https://phplaravel-1087149-3834893.cloudwaysapps.com/api",
   });
 
-  const token = localStorage.getStoredToken(); // Call the function to get the parsed token
+  const token = localStorage.getItem("token"); // Call the function to get the parsed token
   console.log("Stored Token:", token);
   if (token) {
     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;

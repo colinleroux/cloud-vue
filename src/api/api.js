44ace01e32b290d4919819e3bc56ef14c18ded58
Colin le Roux <colinleroux@gmail.com>
2023-08-29 09:10:33 +0800

    login and jwt1

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function () {
   let api = axios.create({
-    baseURL: "https://phplaravel-1087149-3834893.cloudwaysapps.com/api",
+    backendURL: "https://phplaravel-1087149-3834893.cloudwaysapps.com/api",
   });
 
   let token = localStorage.getItem("token");

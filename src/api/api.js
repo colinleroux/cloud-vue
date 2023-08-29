5ce42bd2e6f5bfabc5179cf3bd07a7a34b901087
Colin le Roux <colinleroux@gmail.com>
2023-08-29 17:09:30 +0800

    login and jwt39

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://phplaravel-1087149-3834893.cloudwaysapps.com/api",
-});
+export default function () {
+  let api = axios.create({
+    baseURL: "https://phplaravel-1087149-3834893.cloudwaysapps.com/api",
+  });
 
-const token = localStorage.getItem("token"); // Call the function to get the parsed token
-console.log("Stored Token:", token);
-if (token) {
-  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  console.log("Authorization Header Set with Token:", token);
+  const token = localStorage.getItem("token"); // Call the function to get the parsed token
+  console.log("Stored Token:", token);
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    console.log("Authorization Header Set with Token:", token);
+  }
+  return api;
 }
-export default api;
